9ab464bafbea22d5caf96097451aedb4ff095a37
Aleksander Dudek <aleksander.dudek@yahoo.pl>
2019-03-07 20:45:37 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import {  } from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'triggerForm';
+  title = 'Trigger Form';
+  favoriteSeason: string;
+  seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
 }
